284fd07c9ee4161e61358fba9a2eba828a164fd8
tomyrd <chubyrd1@gmail.com>
2019-11-07 10:32:48 -0300

    Cambio de importaciones en TableroTest

--- src/main/java/test/TableroTest.java
+++ src/main/java/test/TableroTest.java
@@ -7,7 +7,7 @@ import Modelo.Tablero.Tablero;
 import Modelo.Tablero.UnidadAgregadaEnSectorEnemigoExeption;
 import Modelo.Unidad;
 import org.junit.Assert;
-import org.testng.annotations.Test;
+import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TableroTest {
