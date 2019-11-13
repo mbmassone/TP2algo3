9805e7ae996bb29b44edb35fd48fe54fed57002a
Juan Benitez <jbenitez@fi.uba.ar>
2019-11-13 08:20:50 -0300

    Actualizado el demo del JavaFX, agregado de botones. Agregado una interfaz para escenarios con un escenario que es la Pantalla de titulo.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import Modelo.Tablero.Tablero;
+import Vista_api.Pantalla_titulo;
 import javafx.application.Application;
 import javafx.scene.Scene;
 
@@ -9,6 +10,8 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+
+
 import java.io.FileInputStream;
 
 
@@ -16,41 +19,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage DebugStage) throws Exception {
+        DebugStage.setTitle("AlgoChessApp Beta0..0.0.1");
 
-        //Variables a usar
-        int filas, columnas;
-
-        //Inicio el modelo
-        Tablero tablero = new Tablero();
-        /*TODO
-        Como le informo a la interfaz la matriz de casilleros(la lista de casilleros es privada y no hay getter)?
-         */
-
-        //Obtengo sus variables para mostrar
-
-        //Titulo de la ventana
-        DebugStage.setTitle("Java FX tTittle");
-
-        //Tamaño de la ventana del stage
-
-
-        //Setup de elementos a usar
-        //Se abre un archivo
-        FileInputStream PantallaTitulo = new FileInputStream("PantallaTitulo.png");
-        //Creo un objeto imagen a mostrar
-        Image image = new Image(PantallaTitulo);
-        //coloco un visor y lo referencio
-        ImageView imageView = new ImageView(image);
-        Label texto = new Label("Debug Info");
-        VBox vbox = new VBox(20,imageView,texto);
-        ///Creacion de una Escena
-        Scene scene = new Scene(vbox,image.getWidth(),image.getHeight()*2);
-        //Coloco la escena en el Stage
-        DebugStage.setScene(scene);
-
-        //Lo muestro
+        Pantalla_titulo wn = new Pantalla_titulo();
+        wn.actualizar();
+        wn.mostrar(DebugStage);
         DebugStage.show();
-
     }
 
     public static void main(String[] args) {
