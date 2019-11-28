285d5d0fa487ce07ca731fe1afd757b0e2531ca8
Juan Benitez <jbenitez@fi.uba.ar>
2019-11-28 14:06:17 -0300

    Actualizacion de la vista del panel derecho para colocar unidades, aun no funcional.

--- src/main/java/Vista_api/RecursosClass/PanelDerechoColocarUnidades.java
+++ src/main/java/Vista_api/RecursosClass/PanelDerechoColocarUnidades.java
@@ -2,6 +2,7 @@ package Vista_api.RecursosClass;
 
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.TextAlignment;
 
@@ -10,18 +11,29 @@ public class PanelDerechoColocarUnidades extends VBox {
     PanelDerechoColocarUnidades(){
         Label texto_superior = new Label("Por favor, Creen su batallon");
         Label texto_jugador_actual = new Label("Turno actual:");
+        Label jugador_actual = new Label("JUGADOR_ACTUAL");
+        Label texto_puntos_restantes = new Label("Puntos a usar:");
+        Label puntos_restantes = new Label("99");
 
+        Label intrucciones = new Label("\n\nBienvenido/a " + jugador_actual.getText() + " para continuar haga click en una unidad y haga click de nuevo\n donde quiera colocarla, repita este porceso hasta que se acaben\n sus puntos o haga click en el boton Terminar\n\n\n\n");
+
+        HBox banner_jugador = new HBox(texto_jugador_actual,jugador_actual);
+        HBox banner_puntos = new HBox(texto_puntos_restantes, puntos_restantes);
+
+        TableroUnidadesACrear tableroUnidadesACrear = new TableroUnidadesACrear();
 
         texto_superior.setAlignment(Pos.CENTER);
         texto_superior.setTextAlignment(TextAlignment.CENTER);
 
 
         texto_jugador_actual.setAlignment(Pos.CENTER_LEFT);
-        texto_jugador_actual.setTextAlignment(TextAlignment.LEFT);
-
+        jugador_actual.setAlignment(Pos.TOP_RIGHT);
 
         //Agregar cada elemento a si mismo
-        this.getChildren().addAll(texto_superior,texto_jugador_actual);
+        this.getChildren().addAll(texto_superior,banner_jugador,banner_puntos,intrucciones, tableroUnidadesACrear);
+    }
+
+    public void actualizar(){
 
     }
 }
---
+++ src/main/java/Vista_api/RecursosClass/TableroUnidadesACrear.java
@@ -0,0 +1,51 @@
+package Vista_api.RecursosClass;
+
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import static Vista_api.ConstantesInterfaz.*;
+
+public class TableroUnidadesACrear extends HBox {
+
+    ImageView imagen_catapulta;
+    ImageView imagen_curandero;
+    ImageView imagen_infanteria;
+    ImageView imagen_jinete;
+
+    Label texto_catapulta;
+    Label texto_curandero;
+    Label texto_infanteria;
+    Label texto_jinete;
+
+    GridPane matriz;
+
+    public TableroUnidadesACrear(){
+
+        this.matriz = new GridPane();
+        this.matriz.setHgap(20);
+        this.matriz.setVgap(20);
+
+        imagen_catapulta = new ImageView(new Image(IMG_CATAPULTA));
+        imagen_curandero = new ImageView(new Image(IMG_CURANDERO));
+        imagen_infanteria = new ImageView(new Image(IMG_INFANTERIA));
+        imagen_jinete = new ImageView(new Image(IMG_JINETE));
+
+        texto_catapulta = new Label("Catapulta");
+        texto_curandero = new Label("Curandero");
+        texto_infanteria= new Label("Infanteria");
+        texto_jinete= new Label("Jinete");
+
+        this.matriz.add(imagen_catapulta,0,0);
+        this.matriz.add(texto_catapulta,1,0);
+        this.matriz.add(imagen_curandero,0,1);
+        this.matriz.add(texto_curandero,1,1);
+        this.matriz.add(imagen_infanteria,0,2);
+        this.matriz.add(texto_infanteria,1,2);
+        this.matriz.add(imagen_jinete,0,3);
+        this.matriz.add(texto_jinete,1,3);
+
+        this.getChildren().addAll(this.matriz);
+    }
+}
