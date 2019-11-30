2ff657ff1bc3473ba5c933f9967beab304dd62ad
Bernardo <mbmassone@fi.uba.ar>
2019-11-30 16:45:30 -0300

    Correccion agregado de EstadoTerreno

---
+++ src/main/java/Modelo/Casillero/EnTerritorioAliado.java
@@ -0,0 +1,14 @@
+package Modelo.Casillero;
+
+import Modelo.Unidad;
+
+public class EnTerritorioAliado implements EstadoTerritorio {
+    public void analizarDanio(Unidad unidad, float danio){
+        unidad.descontarVida(danio);
+    }
+
+    public void actualizarEstado(Unidad unidad, Casillero casillero){
+        if(casillero.obtenerDuenio().obtenerNombre().equals(unidad.obtenerDuenio().obtenerNombre()))
+            unidad.actualizarEstadoTerritorio(new EnTerritorioEnemigo() );
+    }
+}
