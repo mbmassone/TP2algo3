6cca3487d8ce1268de88226d451e03f4aac268d4
tomyrd <chubyrd1@gmail.com>
2019-11-30 16:34:51 -0300

    Cambios en controladores

--- src/main/java/Controlador/ControladorDeAgregarUnidad.java
+++ src/main/java/Controlador/ControladorDeAgregarUnidad.java
@@ -3,21 +3,31 @@ package Controlador;
 import Controlador.CreadorDeUnidad.CreadorDeUnidad;
 import Modelo.Jugador;
 import Modelo.Tablero.Coordenada;
+import Modelo.Tablero.Tablero;
 import Modelo.Tablero.UnidadAgregadaEnSectorEnemigoExcepcion;
+import Modelo.Turno;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
 
-public class ControladorDeAgregarUnidad {
-    private ContenedorDeClases contenedor;
+public class ControladorDeAgregarUnidad implements EventHandler<ActionEvent> {
+    private Tablero tablero;
+    private Turno turno;
+    private Coordenada coordenada;
+    private CreadorDeUnidad creador;
 
-    public ControladorDeAgregarUnidad(ContenedorDeClases contenedor){
-        this.contenedor = contenedor;
+    public ControladorDeAgregarUnidad(ContenedorDeClases contenedor, Coordenada coordenada, CreadorDeUnidad creador){
+        this.tablero = contenedor.obtenerTablero();
+        this.turno = contenedor.obtenerTurno();
+        this.coordenada = coordenada;
+        this.creador = creador;
     }
 
-    public boolean agregarUnidad(int fila, int columna, CreadorDeUnidad creador, String nombreDuenio){
+    @Override
+    public void handle(ActionEvent actionEvent) {
         try{
-            contenedor.obtenerTablero().agregarUnidad(new Coordenada(fila, columna), creador.crearUnidad(contenedor.obtenerJugador(nombreDuenio)));
-        } catch (Exception excepcion){
-            return false;
+            tablero.agregarUnidad(coordenada, creador.crearUnidad(turno.obtenerJugadorActual()));
+        } catch (Exception exception){
+            System.out.println("No se puede agregar ahí"); //TODO avisar a usuario.
         }
-        return true;
     }
 }
